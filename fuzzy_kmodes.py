5cd1d91f2656a195773b9290568ed6129a39bc31
Darshan Mehta <darshanmehta17@gmail.com>
2016-06-08 16:55:17 +0530

    Added function to calculate partition matrix

--- fuzzy_kmodes.py
+++ fuzzy_kmodes.py
@@ -37,7 +37,7 @@ def calculate_cost(W, Z, alpha):
     Calculates the cost function of k-modes algorithm as per Huang '99 paper on fuzzy k-modes.
     :param W: Fuzzy partition matrix
     :param Z: Cluster centroids
-    :param alpha: weighing exponent
+    :param alpha: Weighing exponent
     :return: Cost of of the current setup using the formula from the paper
     """
     k = W.shape[0]
@@ -50,6 +50,46 @@ def calculate_cost(W, Z, alpha):
             cost += pow(W[l][i], alpha) * calculate_dissimilarity(Z[l], X[i])
 
     return cost
+
+
+def calculate_partition_matrix(Z, X, alpha):
+    """
+    Calculates the dissimilarity matrix W for a fixed Z as per Theorem 1 in Huang '99 paper on fuzzy kmodes.
+    :param Z: Fixed centroids
+    :param X: Dataset points
+    :param alpha: Weighing exponent
+    :return: Dissimilarity matrix of type Numpy array of dimension k x n.
+    """
+    k = Z.shape[0]
+
+    n = X.shape[0]
+
+    exponent = 1 / (alpha - 1)
+
+    W = np.zeros((k, n), dtype='float')
+
+    for l in range(k):
+        for i in range(n):
+            dli = calculate_dissimilarity(Z[l], X[i])
+            if dli == 0:
+                W[l][i] = 1
+            else:
+                flag = False
+                sum = 0
+                for h in range(k):
+                    dhi = calculate_dissimilarity(Z[h], X[i])
+
+                    if h != l and dhi == 0:
+                        W[l][i] = 0
+                        flag = True
+                        break
+
+                    sum += pow(dli / dhi, exponent)
+
+                if not flag:
+                    W[l][i] = 1 / sum
+
+    return W
 
 
 def fuzzy_kmodes(X, n_clusters=4):
