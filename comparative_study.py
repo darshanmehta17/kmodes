df5b53171817b39aefd1c2376317d0fb3852cfde
Darshan Mehta <darshanmehta17@gmail.com>
2016-06-14 01:52:47 +0530

    Performed a comparative study on Yager and Fuzzy K mode

--- comparative_study.py
+++ comparative_study.py
@@ -1,9 +1,9 @@
 import fuzzy_kmodes
-import intuitionistic_fuzzy_kmodes
+import intuition_yager
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(1.1, 20, 0.2)
+x = np.arange(1.1, 10, 0.2)
 
 y_fuzzy = np.zeros((x.shape[0]), dtype='float')
 
@@ -18,7 +18,7 @@ n_clusters = 4
 for i in range(len(x)):
     print "Iteration:", i + 1
     a, b, y_fuzzy[i] = fuzzy_kmodes.run(n_iter, n_clusters, x[i])
-    a, b, y_intuitionistic[i] = intuitionistic_fuzzy_kmodes.run(n_iter, n_clusters, x[i])
+    a, b, y_intuitionistic[i] = intuition_yager.run(n_iter, n_clusters, x[i])
 
 plt.plot(x, y_fuzzy)
 plt.plot(x, y_intuitionistic)
