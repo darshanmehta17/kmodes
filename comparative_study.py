04bd87bfe0c9843c8a93395c7aa9347a761a4c51
Darshan Mehta <darshanmehta17@gmail.com>
2016-06-27 18:20:58 +0530

    Updated graph style

--- comparative_study.py
+++ comparative_study.py
@@ -4,13 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-alphas = np.arange(1.1, 3, 0.2)
+alphas = np.arange(1.1, 3.0, 0.2)
 
 # Importing data from data set and reformatting into attributes and labels
-x = np.genfromtxt('soybean.csv', dtype=str, delimiter=',')[:, :-1]
-y = np.genfromtxt('soybean.csv', dtype=str, delimiter=',', usecols=(21,))
-# x = np.genfromtxt('zoo.csv', dtype=str, delimiter=',')[:, :-1]
-# y = np.genfromtxt('zoo.csv', dtype=str, delimiter=',', usecols=(17,))
+# x = np.genfromtxt('soybean.csv', dtype=str, delimiter=',')[:, :-1]
+# y = np.genfromtxt('soybean.csv', dtype=str, delimiter=',', usecols=(21,))
+x = np.genfromtxt('zoo.csv', dtype=str, delimiter=',')[:, :-1]
+y = np.genfromtxt('zoo.csv', dtype=str, delimiter=',', usecols=(17,))
+# x = np.genfromtxt('saturday.csv', dtype=str, delimiter=',')[:, :-1]
+# y = np.genfromtxt('saturday.csv', dtype=str, delimiter=',', usecols=(4,))
+# x = np.genfromtxt('credit.csv', dtype=str, delimiter=',')[:, :-1]
+# y = np.genfromtxt('credit.csv', dtype=str, delimiter=',', usecols=(15,))
 
 y_fuzzy_accuracy = np.zeros((alphas.shape[0]), dtype='float')
 y_fuzzy_db = np.zeros((alphas.shape[0]), dtype='float')
@@ -24,7 +28,7 @@ y_intuitionistic_dunn = np.zeros((alphas.shape[0]), dtype='float')
 n_iter = 50
 
 # Number of clusters
-n_clusters = 4
+n_clusters = 7
 
 print "######### Starting Iterations ##########"
 print
@@ -60,27 +64,53 @@ print "Intuitionistic Fuzzy k-modes:", sum(y_intuitionistic_dunn) / len(y_intuit
 
 
 plt.figure(1)
-plt.plot(alphas, y_fuzzy_accuracy)
-plt.plot(alphas, y_intuitionistic_accuracy)
+ax = plt.subplot(111)
+ax.plot(alphas, y_fuzzy_accuracy, color='r', linestyle='--', label='Fuzzy k-modes')
+ax.plot(alphas, y_intuitionistic_accuracy, color='b', label='Intuitionistic Fuzzy k-modes')
 plt.xlabel('Alpha')
 plt.ylabel('Accuracy')
 plt.title('Comparative Study - Accuracy')
-plt.legend(['Fuzzy K-modes', 'Intuitionistic Fuzzy K-modes'])
+
+# Shrink current axis's height by 10% on the bottom
+box = ax.get_position()
+ax.set_position([box.x0, box.y0 + box.height * 0.1,
+                 box.width, box.height * 0.9])
+
+# Put a legend below current axis
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
+          fancybox=True, shadow=True, ncol=2)
+
 
 plt.figure(2)
-plt.subplot(2, 1, 1)
-plt.plot(alphas, y_fuzzy_db)
-plt.plot(alphas, y_intuitionistic_db)
+ax2 = plt.subplot(111)
+ax2.plot(alphas, y_fuzzy_db, color='r', linestyle='--', label='Fuzzy k-modes')
+ax2.plot(alphas, y_intuitionistic_db, color='b', label='Intuitionistic Fuzzy k-modes')
 plt.xlabel('Alpha')
 plt.ylabel('DB Index')
 plt.title('Comparative Study - DB Index')
-plt.legend(['Fuzzy K-modes', 'Intuitionistic Fuzzy K-modes'])
+# Shrink current axis's height by 10% on the bottom
+box = ax2.get_position()
+ax2.set_position([box.x0, box.y0 + box.height * 0.1,
+                 box.width, box.height * 0.9])
 
-plt.subplot(2, 1, 2)
-plt.plot(alphas, y_fuzzy_dunn)
-plt.plot(alphas, y_intuitionistic_dunn)
+# Put a legend below current axis
+ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
+          fancybox=True, shadow=True, ncol=2)
+
+plt.figure(3)
+ax2 = plt.subplot(111)
+ax2.plot(alphas, y_fuzzy_dunn, color='r', linestyle='--', label='Fuzzy k-modes')
+ax2.plot(alphas, y_intuitionistic_dunn, color='b', label='Intuitionistic Fuzzy k-modes')
 plt.xlabel('Alpha')
 plt.ylabel('Dunn Index')
 plt.title('Comparative Study - Dunn Index')
-plt.legend(['Fuzzy K-modes', 'Intuitionistic Fuzzy K-modes'])
+# Shrink current axis's height by 10% on the bottom
+box = ax2.get_position()
+ax2.set_position([box.x0, box.y0 + box.height * 0.1,
+                 box.width, box.height * 0.9])
+
+# Put a legend below current axis
+ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
+          fancybox=True, shadow=True, ncol=2)
+
 plt.show()
